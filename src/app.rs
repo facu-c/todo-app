a644be0bbd19a9ceb757e782dcc91e7f3e0a6edc
Facundo Caniza <facundocaniza@proton.me>
2025-03-03 15:30:36 -0300

    empezando a manejar errores

--- src/app.rs
+++ src/app.rs
@@ -1,33 +1,47 @@
-use create_task::get_task;
-
 
 mod create_task {
-    use std::{fs::{self, File}, io::{self, Write}};
+    use std::{fs::File, io::{self, Write}};
 
-    fn create_file(input : &String) {
+    fn create_file(input_task : &String, input_name: &String) {
+
+        println!("**********");
         
-        let mut f = File::create("//home/facundo/Documentos/foo.txt").expect("No se pudo crear el archivo");
+        let mut f = File::create(format!("//home/facundo/Documentos/{}.txt",input_name)).expect("No se pudo crear el archivo");
+        match f.write_all(&input_task.as_bytes()) {
+            Ok(_) => {
+                println!("Archivo creado con exito");
+            },
+            Err(msg) => {
+                eprintln!("No se pudo crear el archivo: {}", msg);
+            }
+        }
 
-        let _ = f.write_all(&input.as_bytes());
-
+        println!("**********");
 
     }
 
     pub fn get_task() {
-        let mut input = String::new();
+        
+        let mut input_task = String::new();
+        let mut input_name = String::new();
+        
+        print!("Ingrese el nombre con el que guardar el archivo: ");
+        let _ = io::stdin().read_line(&mut input_name);
+        println!();
+        print!("Ingrese la tarea a realizar: ");
+        let _ = io::stdin().read_line(&mut input_task);
+        
+        println!();
 
-        println!("Ingrese la tarea a realizar: ");
-
-        let _ = io::stdin().read_line(&mut input);
-
-        create_file(&input);
+        create_file(&input_task, &input_name);
     }
 }
 
-
 pub fn run_app() {
+    
+    use create_task::get_task;
 
-    println!("desde run_app");
+    println!();
     
     get_task();
 }
