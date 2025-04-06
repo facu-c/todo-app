7d19dcf5c9f41c6be67e08b5b6d72a87cb98507a
Facundo Caniza <facundocaniza@proton.me>
2025-04-06 14:27:02 -0300

    arreglo del manejo del ID

--- src/modulos/menu.rs
+++ src/modulos/menu.rs
@@ -1,14 +1,13 @@
-
-use std::io;
 use crate::modulos::tasks::{create_task, list_task, load_task, make_done, remove_task};
+use std::io;
 
 pub fn start_menu() {
-
     let mut tasks = load_task();
-    let mut id_number = 0;
-
+    let mut id_number = match tasks.last() {
+        Some(task) => task.id_task + 1,
+        None => 0,
+    };
     loop {
-        println!();
         println!("Ingrese el numero de tarea a realizar: ");
         println!("A. Ingresar nueva tarea.");
         println!("B. Listar tareas");
@@ -16,27 +15,27 @@ pub fn start_menu() {
         println!("Y. Marcar Hecha");
         println!("X. Salir de Qacer");
 
-        let mut number_option = String::new();        
-        
+        let mut number_option = String::new();
+
         match io::stdin().read_line(&mut number_option) {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(_) => eprintln!("No se logro identificar la accion solicitada"),
         }
         match number_option.trim().to_uppercase().as_str() {
-            
             "X" => break,
             "A" => {
-                    create_task(&mut tasks, id_number);
-                    id_number += 1;
-                }
+                create_task(&mut tasks, id_number);
+                id_number += 1;
+            }
             "B" => list_task(),
             "D" => remove_task(&tasks),
             "Y" => make_done(&tasks),
             _ => {
-                println!("No se logro identificar la accion solicitada, por favor, intente de nuevo");
+                println!(
+                    "No se logro identificar la accion solicitada, por favor, intente de nuevo"
+                );
                 continue;
-            },
-            //TODO: Agregar "recuperar tareas"
+            }
         }
     }
     println!("¡Hasta luego!");
--- src/modulos/tasks.rs
+++ src/modulos/tasks.rs
@@ -4,18 +4,25 @@ use crate::modulos::config::ConfigFile;
 use std::io;
 
 pub fn create_task(tasks: &mut Vec<ConfigFile>, id_task: u64) {
-    
-    let path = format!("{}/qacer/save_file",dirs::document_dir().unwrap().display());
+    let path = format!(
+        "{}/qacer/save_file",
+        dirs::document_dir().unwrap().display()
+    );
     let mut input_desc_task = String::new();
     let mut input_name_task = String::new();
-    
+
     println!("Ingrese de la tarea: ");
     let _ = io::stdin().read_line(&mut input_name_task);
     println!();
     println!("Agrege una descripcion de la tarea: ");
     let _ = io::stdin().read_line(&mut input_desc_task);
 
-    tasks.push(ConfigFile { id_task, name_task: input_name_task.trim().to_string(), desc_task: input_desc_task.trim().to_string(), done: false} );
+    tasks.push(ConfigFile {
+        id_task,
+        name_task: input_name_task.trim().to_string(),
+        desc_task: input_desc_task.trim().to_string(),
+        done: false,
+    });
 
     let json = serde_json::to_string_pretty(tasks).unwrap();
     fs::write(path, json).unwrap();
@@ -24,25 +31,36 @@ pub fn create_task(tasks: &mut Vec<ConfigFile>, id_task: u64) {
 }
 
 pub fn load_task() -> Vec<ConfigFile> {
-    let path = format!("{}/qacer/save_file",dirs::document_dir().unwrap().display());
+    let path = format!(
+        "{}/qacer/save_file",
+        dirs::document_dir().unwrap().display()
+    );
     let data = fs::read_to_string(path).unwrap_or_else(|_| "[]".to_string());
     serde_json::from_str(&data).unwrap_or_default()
-    
 }
 
 pub fn list_task() {
-    let path = format!("{}/qacer/save_file",dirs::document_dir().unwrap().display());
+    let path = format!(
+        "{}/qacer/save_file",
+        dirs::document_dir().unwrap().display()
+    );
     let data = fs::read_to_string(path).unwrap_or_else(|_| "[]".to_string());
     let tasks: Vec<ConfigFile> = serde_json::from_str(&data).unwrap_or_default();
 
     for element in tasks {
-        println!("{}: {} - {} - {}", element.id_task, element.name_task, element.desc_task, element.done);
+        println!(
+            "{}: {} - {} - {}",
+            element.id_task, element.name_task, element.desc_task, element.done
+        );
         println!("---------------");
     }
 }
 
-pub fn remove_task( tasks: &[ConfigFile]) {
-    let path = format!("{}/qacer/save_file",dirs::document_dir().unwrap().display());
+pub fn remove_task(tasks: &[ConfigFile]) {
+    let path = format!(
+        "{}/qacer/save_file",
+        dirs::document_dir().unwrap().display()
+    );
     let mut new_data: Vec<ConfigFile> = vec![];
     let mut id_delete = String::new();
 
@@ -63,7 +81,10 @@ pub fn remove_task( tasks: &[ConfigFile]) {
 }
 
 pub fn make_done(tasks: &[ConfigFile]) {
-    let path = format!("{}/qacer/save_file",dirs::document_dir().unwrap().display());
+    let path = format!(
+        "{}/qacer/save_file",
+        dirs::document_dir().unwrap().display()
+    );
     let mut new_data: Vec<ConfigFile> = vec![];
     let mut id_done = String::new();
 
